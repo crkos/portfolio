746f24ec0fe50c8e52bf69d6c506219228ef743e
Jordan Higuera Higuera <jordandark123@gmail.com>
2023-07-25 19:07:42 -0700

    Delete click on image to redirect, add button to reditect on image

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,7 +2,7 @@
 import React, {FC, MouseEventHandler} from 'react';
 import Image from "next/image";
 import {Projects} from "@/types/types";
-import {Expand, Github} from "lucide-react";
+import {Expand, Github, LucideExternalLink} from "lucide-react";
 import IconButton from "@/components/ui/IconButton";
 import usePreviewModal from "@/hooks/UsePreviewModal";
 
@@ -21,11 +21,12 @@ const ProjectCard: FC<Projects> = ({title, tags, img, link, gitLink}) => {
         <section className="p-1 group cursor-pointer rounded-xl shadow-md h-full ">
             <article className="rounded-xl bg-white p-3 duration-150 hover:scale-105 hover:transform hover:shadow-xl dark:text-black relative h-full">
                 <div className="relative flex items-end overflow-hidden rounded-xl aspect-video">
-                    <a href={link} target="_blank" rel="noopener"><Image src={img[0].url} alt="Ecommerce store" decoding="async" loading="lazy" height={600} width={600} quality={75} fetchPriority="low"/></a>
+                    <Image src={img[0].url} alt="Ecommerce store" decoding="async" loading="lazy" height={600} width={600} quality={75} fetchPriority="low"/>
                     <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-2">
                         <div className="flex gap-x-6 justify-end">
                             <IconButton icon={<Expand size={20} className="text-gray-600" aria-label="Abrir galeria"/>} onClick={onPreview} />
                             <a href={gitLink}  target="_blank" rel="noopener"><IconButton icon={<Github size={20} className="text-gray-600" aria-label="Abrir galeria"/>}/></a>
+                            <a href={link}  target="_blank" rel="noopener"><IconButton icon={<LucideExternalLink size={20} className="text-gray-600" aria-label="Ir al demo"/>}/></a>
                         </div>
                     </div>
                 </div>
